14f59ab7789be6188e2ad5849e545b97598dda41
dgrat <dan.frenzel87@gmail.com>
2022-04-29 06:38:11 -0700

    Signed-off-by: dgrat <dan.frenzel87@gmail.com>

--- src/scripts/pick_objects.sh
+++ src/scripts/pick_objects.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Launch the nodes
-xterm  -e "source devel/setup.bash; export TURTLEBOT_GAZEBO_WORLD_FILE="$(pwd)/src/world/home_service_robot.world"; roslaunch turtlebot_gazebo turtlebot_world.launch" &
+xterm  -e "source devel/setup.bash; export TURTLEBOT_GAZEBO_WORLD_FILE="$(pwd)/src/world/office.world"; roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 7
-xterm  -e "source devel/setup.bash; export TURTLEBOT_GAZEBO_MAP_FILE="$(pwd)/src/world/home_service_robot.yaml"; roslaunch turtlebot_gazebo amcl_demo.launch " &
+xterm  -e "source devel/setup.bash; export TURTLEBOT_GAZEBO_MAP_FILE="$(pwd)/src/world/office.yaml"; roslaunch turtlebot_gazebo amcl_demo.launch " &
 sleep 5
 xterm  -e "source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
